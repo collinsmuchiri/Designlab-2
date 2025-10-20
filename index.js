ab4b61bfc440e6cd346eca0f2de1d4add9c50e38
collinsmuchiri <149315952+collinsmuchiri@users.noreply.github.com>
2025-10-20 22:32:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+//PLUGIN REGISTER
+gsap.registerPlugin(ScrollTrigger);
+
+//ANIMATIONS
+gsap.to(".welcom", {
+    rotation: 360,
+    duration: 4,
+    repeat: -1,
+    ease: "power3.inOut"
+}) 
+
+gsap.to (".servicestxt", {
+    y: 0,
+    duration: 2,
+
+    scrollTrigger: {
+        trigger: ".servicestxt",
+        start: "top 80%",
+    markers: true},
+
+    opacity: 1,
+    ease: "power3.inOut"}) 
+    
+gsap.to (".graphics", {
+    y: 0,
+    duration: 2,
+
+    scrollTrigger: {
+        trigger: ".graphics",
+        start: "top 80%",
+    markers: true},
+
+    opacity: 1,
+    ease: "power3.inOut"})
+    
+gsap.to (".imaginecreate", {
+    y: 0,
+    duration: 2.5,
+
+    scrollTrigger: {
+        trigger: ".imaginecreate",
+        start: "top 90%",
+    markers: true},
+
+    opacity: 1,
+    ease: "power3.inOut"})
+
+gsap.to (".imaginecreatemobile", {
+    y: 0,
+    duration: 2.5,
+
+    scrollTrigger: {
+        trigger: ".imaginecreatemobile",
+        start: "top 90%",
+    markers: true},
+
+    opacity: 1,
+    ease: "power3.inOut"})
+    
+gsap.to (".scroll", {
+    y: 0,
+    duration: 3,
+    repeat: -1,
+
+    scrollTrigger: {
+        trigger: ".scroll",
+        start: "top 90%",
+    markers: true},
+
+    opacity: 1,
+    ease: "power3.inOut"})    
+    
+gsap.to (".floatingmac", {
+    y: 0,
+    duration: 2,
+
+    scrollTrigger: {
+        trigger: ".floatingmac",
+        start: "top 90%",
+    markers: true},
+
+    opacity: 1,
+    ease: "power3.inOut"})
