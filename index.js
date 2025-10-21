6e945b183a34c4751f0db1da38575571a63ea474
collinsmuchiri <149315952+collinsmuchiri@users.noreply.github.com>
2025-10-21 17:25:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,122 @@
-//PLUGIN REGISTER
-gsap.registerPlugin(ScrollTrigger);
+// REGISTER PLUGINS
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-//ANIMATIONS
-gsap.to(".welcom", {
-    rotation: 360,
-    duration: 4,
-    repeat: -1,
-    ease: "power3.inOut"
-}) 
+//SCROLL SMOOTHER
+    const smoother = ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1.5,
+      effects: true
+    });
 
-gsap.to (".servicestxt", {
-    y: 0,
-    duration: 2,
+//LAG
+smoother.effects(".largetxt, .brandidentity, .text2",{ 
+  lag: 0.3
+});    
 
-    scrollTrigger: {
+// ANIMATIONS
+    gsap.to(".welcom", {
+      rotation: 360,
+      duration: 4,
+      repeat: -1,
+      ease: "power3.inOut"
+    });
+
+    gsap.to(".servicestxt", {
+      y: 0,
+      duration: 2,
+      scrollTrigger: {
         trigger: ".servicestxt",
-        start: "top 80%"},
+        start: "top 80%",
+        markers: true
+      },
+      opacity: 1,
+      ease: "power3.inOut"
+    });
 
-    opacity: 1,
-    ease: "power3.inOut"}) 
-    
-gsap.to (".graphics", {
-    y: 0,
-    duration: 2,
-
-    scrollTrigger: {
+    gsap.to(".graphics", {
+      y: 0,
+      duration: 2,
+      scrollTrigger: {
         trigger: ".graphics",
-        start: "top 80%"},
+        start: "top 80%",
+        markers: true
+      },
+      opacity: 1,
+      ease: "power3.inOut"
+    });
 
-    opacity: 1,
-    ease: "power3.inOut"})
-    
-gsap.to (".imaginecreate", {
-    y: 0,
-    duration: 2.5,
-
-    scrollTrigger: {
+    gsap.to(".imaginecreate", {
+      y: 0,
+      duration: 2.5,
+      scrollTrigger: {
         trigger: ".imaginecreate",
-        start: "top 90%"},
+        start: "top 90%",
+        markers: true
+      },
+      opacity: 1,
+      ease: "power3.inOut"
+    });
 
-    opacity: 1,
-    ease: "power3.inOut"})
-
-gsap.to (".imaginecreatemobile", {
-    y: 0,
-    duration: 2.5,
-
-    scrollTrigger: {
+    gsap.to(".imaginecreatemobile", {
+      y: 0,
+      duration: 2.5,
+      scrollTrigger: {
         trigger: ".imaginecreatemobile",
-        start: "top 90%"},
+        start: "top 90%",
+        markers: true
+      },
+      opacity: 1,
+      ease: "power3.inOut"
+    });
 
-    opacity: 1,
-    ease: "power3.inOut"})
-    
-gsap.to (".scroll", {
-    y: 0,
-    duration: 3,
-    repeat: -1,
-
-    scrollTrigger: {
+    gsap.to(".scroll", {
+      y: 0,
+      duration: 3,
+      repeat: -1,
+      scrollTrigger: {
         trigger: ".scroll",
-        start: "top 90%"},
+        start: "top 90%",
+        markers: true
+      },
+      opacity: 1,
+      ease: "power3.inOut"
+    });
 
-    opacity: 1,
-    ease: "power3.inOut"})    
-    
-gsap.to (".floatingmac", {
-    y: 0,
-    duration: 2,
-
-    scrollTrigger: {
+    gsap.to(".floatingmac", {
+      y: 0,
+      duration: 2,
+      scrollTrigger: {
         trigger: ".floatingmac",
-        start: "top 90%"},
+        start: "top 90%",
+        markers: true
+      },
+      opacity: 1,
+      ease: "power3.inOut"
+    });
 
-    opacity: 1,
-    ease: "power3.inOut"})
+    gsap.to(".projectcontent", {
+      y: 0,
+      duration: 2,
+      opacity: 1,
+      ease: "power3.inOut",
+      
+      scrollTrigger: {
+        trigger: ".ourwork",
+        start: "top 80%",
+        markers: true},});
 
+    gsap.to(".servicestxt", {
+      y: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".servicestxt",
+        start: "top 80%",
+        markers: true
+      },
+      opacity: 1,
+      ease: "power3.inOut"
+    });
+
+    // refresh after setup
+    ScrollTrigger.refresh();
